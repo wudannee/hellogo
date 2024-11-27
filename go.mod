83d1e0ad98e8b1e59a40f71a677609063718623d
wudannee <>
2024-11-27 13:55:00 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module hellogo
+
+go 1.23.3
+
+require golang.org/x/exp v0.0.0-20241108190413-2d47ceb2692f
---
+++ m1.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+type Student struct {
+	XMLName    xml.Name
+	Name       string
+	Age        int
+	Department string
+}
+
+type Mode int
+
+const (
+	QUICK_MODE Mode = iota
+	NORMAL_MODE
+)
+
+func demo_xml(mode Mode) {
+	file, err := os.Open("student.xml")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	var student Student
+
+	switch mode {
+	case QUICK_MODE:
+		if err = xml.NewDecoder(file).Decode(&student); err != nil {
+			log.Fatalln(err)
+		}
+	case NORMAL_MODE:
+		data, err := io.ReadAll(file)
+		if err != nil {
+			fmt.Println("Error reading file:", err)
+			return
+		}
+
+		err = xml.Unmarshal(data, &student)
+		if err != nil {
+			fmt.Println("Error unmarshalling XML:", err)
+			return
+		}
+	default:
+		log.Fatalln("Invalid mode to parse xml")
+	}
+
+	fmt.Println("Parsed Student Info:")
+	fmt.Printf("Name: %s\n", student.Name)
+	fmt.Printf("Age: %d\n", student.Age)
+	fmt.Printf("Department: %s\n", student.Department)
+
+}
+
