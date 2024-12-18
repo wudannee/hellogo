fee449af5064b10792e80468e4f3c8b30329698e
wudannee <188461457+wudannee@users.noreply.github.com>
2024-12-18 15:21:40 +0800

    array

--- ch4/a.go
+++ ch4/a.go
@@ -2,9 +2,40 @@ package ch4
 
 import "fmt"
 
-func DemoArray() {
-	arr1 := [3]int{1, 2, 3}
-	arr2 := [...]int{3, 2, 1}
-	fmt.Println(arr1, arr2)
-	fmt.Println("demo array 6.3")
+type Person struct {
+	Name string
+	Age  int
 }
+
+func DemoArray() {
+	intArr := [3]int{1, 2, 3}
+	intArr2 := [...]int{3, 2, 1}
+	fmt.Println(intArr, intArr2)
+
+	var floatArr = [...]float64{1.1, 2.2, 3.3}
+	var total float64
+	for _, v := range floatArr {
+		total += v
+	}
+	fmt.Println("total:", total)
+
+	var personArr = [5]Person{
+		{Name: "Suan", Age: 16},
+		{Name: "John", Age: 18},
+	}
+
+	// test the copy of an array
+	var personArrCopy [5]Person
+
+	personArrCopy = personArr
+	// modify the original array
+	personArr[0].Name = "Andy"
+	// modify the copied array
+	personArrCopy[0].Name = "Bob"
+
+	// print the original array and the copied array
+	fmt.Println("modified personArr:", personArr)
+	fmt.Println("modified personArrCopy:", personArrCopy)
+
+}
+
