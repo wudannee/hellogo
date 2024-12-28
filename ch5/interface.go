76a448265b504bde88b05bd0d0a9085ccc9e576e
wudannee <188461457+wudannee@users.noreply.github.com>
2024-12-28 20:29:16 +0800

    polymorphism

--- ch5/interface.go
+++ ch5/interface.go
@@ -1,15 +1,12 @@
 package ch5
 
 import (
+	"bufio"
+	"fmt"
 	"io"
 	"os"
+	"strings"
 )
-
-func DemoInterface() {
-	x := &X{true}
-	io.Copy(os.Stdout, x)
-
-}
 
 type X struct {
 	NewLine bool
@@ -26,4 +23,59 @@ func (x *X) Read(p []byte) (n int, err error) {
 
 func (x *X) Write(p []byte) (n int, err error) {
 	return 0, io.EOF
+}
+
+func DemoInterface() {
+	x := &X{true}
+	io.Copy(os.Stdout, x)
+
+	foo1()
+	foo2()
+}
+
+func foo1() {
+	newWriter1 := bufio.NewWriter(os.Stdout)
+	newWriter1.WriteString("bufio writer")
+	newWriter1.Flush()
+
+	fmt.Println("\n---")
+	strBuilder1 := &strings.Builder{}
+	strBuilder1.WriteString("string builder 1")
+	strBuilder1.WriteString("\n string builder 2")
+	fmt.Fprintln(strBuilder1, "\n string builder 3")
+	fmt.Println("buf:", strBuilder1.String())
+}
+
+type Notifier interface {
+	Notify()
+}
+
+type Alarm struct{}
+
+func (a *Alarm) Notify() {
+	fmt.Println("alarm is ringing")
+}
+
+type Phone struct{}
+
+func (p *Phone) Notify() {
+	fmt.Println("phone is ringing")
+}
+
+func foo2() {
+	var notifiers = []Notifier{
+		&Alarm{},
+		&Phone{},
+
+		// will not compile because Notify has a pointer as its receiver
+		// Phone{},
+	}
+
+	notify := func(n Notifier) {
+		n.Notify()
+	}
+
+	for _, n := range notifiers {
+		notify(n)
+	}
 }
